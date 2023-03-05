02a581f2f23466ebf36da37b590bb57d00c7816e
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-05 05:44:31 +0100

    Testing Details

---
+++ api_detials.js
@@ -0,0 +1,3 @@
+let detailsGrid = document.querySelector(".country-details");
+let borderCountries;
+byFields = `?fields=flag,name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders`;
