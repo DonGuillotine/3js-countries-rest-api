206e6854aab72aa7a15fe73b45e4696ca06ed5fa
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-06 22:55:33 +0100

    Attempting to sent a GET request to API III

--- api_V2.js
+++ api_V2.js
@@ -1,4 +1,4 @@
-const baseApiLink = `https://restcountries.com/v3.1/`,
+const baseApiLink = `https://restcountries.com/v2/`,
   all = "all",
   byRegion = `region/`,
   byName = `name/`,
@@ -11,14 +11,14 @@ let byFields = `?fields=name,population,region,capital,flags`,
 function countryStructure(data) {
     return `
     <div class="mx-auto max-w-sm bg-transparent border-gray-200 rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
-        <a href="#" data-country-name="${data.name}>
-            <img class="rounded-t-lg opacity-20" src=${data.flags.svg} alt="${data.name} FLag" />
+        <a href="#" data-country-name="${data.name}">
+            <img class="rounded-t-lg " src=${data.flags.svg} alt="${data.name} FLag" />
         </a>
         <div class="p-5">
             <a href="#">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">${data.name}</h5>
             </a>
-            <ul class="max-w-md space-y-1 list-disc list-inside dark:text-gray-400">
+            <ul class="max-w-md text-white space-y-1 list-disc list-inside dark:text-gray-400">
                 <li><strong>population: </strong>${data.population}</li>
                 <li><strong>Region: </strong>${data.region}</li>
                 <li><strong>capital: </strong>${data.capital}</li>
@@ -73,3 +73,5 @@ async function getCountries(query, limit = 50, getRest = false) {
     //   );
     }
   }
+
+  getCountries(`${all}${byFields}`);
